507056050ba8a04d8adb10d6444d85186953c894
6loccee <martiin17jr@gmail.com>
2024-07-05 00:48:26 +0200

    feat: wordle running simple

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
-import random
-import requests
-import pickle
 import os
+import time
+import random
+import pickle
+import requests
+from collections import Counter
+from colorama import Back, Style, init
 
 CACHE_FILE = 'cache.pickle'
-NUMBER_OF_POKEMONES = 151
+NUMBER_OF_POKEMONS = 151
 POKEMON_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
-
 POKEMON_CACHE = {}
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def load_cache():
     global POKEMON_CACHE
@@ -20,22 +25,108 @@ def save_cache():
         pickle.dump(POKEMON_CACHE, f)
 
 def random_pokemon_name():
-    load_cache()
+  load_cache()
+
+  pokemon_id = random.randint(1, NUMBER_OF_POKEMONS)
+
+  if pokemon_id in POKEMON_CACHE:
+    return POKEMON_CACHE[pokemon_id]
+
+  try:
+    response = requests.get(f'{POKEMON_API_URL}{pokemon_id}')
+    response.raise_for_status()
+    pokemon_data = response.json()
+    pokemon_name = pokemon_data['name']
     
-    pokemon_id = random.randint(1, NUMBER_OF_POKEMONES)
+    POKEMON_CACHE[pokemon_id] = pokemon_name
+    save_cache()
     
-    if pokemon_id in POKEMON_CACHE:
-        return POKEMON_CACHE[pokemon_id]
-    
-    try:
-        response = requests.get(POKEMON_API_URL + pokemon_id)
-        response.raise_for_status()
-        pokemon_data = response.json()
-        pokemon_name = pokemon_data['name']
+    return pokemon_name
+  except requests.RequestException as e:
+    return None
+
+def color_bg(letter, color):
+    colors = {'green': Back.GREEN, 'yellow': Back.YELLOW, 'red': Back.RED}
+    return f'{colors.get(color, Back.RED)} {letter} {Style.RESET_ALL}'
+
+def wordle_result(guess, target):
+    result = []
+    target_counter = Counter(target)
+    guess_counter = Counter(guess)
+    color_map = {
+        (True, True): 'green',
+        (True, False): 'yellow',
+        (False, True): 'red',
+        (False, False): 'red'
+    }
+
+    for guess_letter, target_letter in zip(guess, target):
+        correct_guess = guess_letter == target_letter
+        correct_position = target_counter[guess_letter] > 0
         
-        POKEMON_CACHE[pokemon_id] = pokemon_name
-        save_cache()
+        color = color_map.get((correct_guess, correct_position), 'red')
+        result.append(color_bg(guess_letter, color))
         
-        return pokemon_name
-    except requests.RequestException as e:
-        return None
+        if correct_guess:
+            target_counter[target_letter] -= 1
+        if correct_position:
+            guess_counter[guess_letter] -= 1
+
+    return ' '.join(result)
+
+def main():
+  attempts = 1
+  guesses = []
+  target = random_pokemon_name()
+  pokemon_length = len(target)
+  print('''
+                                  ,' 
+    _.----.        ____         ,'  _\   ___    ___     ____
+_,-'       `.     |    |  /`.   \,-'    |   \  /   |   |    \  |`.
+\      __    \    '-.  | /   `.  ___    |    \/    |   '-.   \ |  |
+ \.    \ \   |  __  |  |/    ,','_  `.  |          | __  |    \|  |
+   \    \/   /,' _`.|      ,' / / / /   |          ,' _`.|     |  |
+    \     ,-'/  /   \    ,'   | \/ / ,`.|         /  /   \  |     |
+     \    \ |   \_/  |   `-.  \    `'  /|  |    ||   \_/  | |\    |
+      \    \ \      /       `-.`.___,-' |  |\  /| \      /  | |   |
+       \    \ `.__,'|  |`-._    `|      |__| \/ |  `.__,'|  | |   |
+        \_.-'       |__|    `-._ |              '-.|     '-.| |   |
+                                `'                            '-._|
+  ''')
+
+  print(f'pokemon has {pokemon_length} letters')
+  time.sleep(3)
+  while attempts > 0:
+    cls()
+
+    for guess, result in guesses:
+      print(result + '\n')
+    
+    guess = input(f'\nthe pokemon is ({attempts} Attempts): ').lower()
+
+    if len(guess) != pokemon_length:
+      print(f'\npokemon has {pokemon_length} letters')
+      time.sleep(1)
+      continue
+
+    result = wordle_result(guess, target)
+    guesses.append((guess, result))
+    attempts -= 1
+
+    if guess == target:
+      cls()
+      for guess, result in guesses:
+        print(result)
+      print(f'\ncongratulations the pokemon was {target}')
+      break
+
+
+    if attempts == 0:
+      print(f'\nyou ran out of attempts the pokemon is: {target}')
+      time.sleep(1)
+      cls()
+      for guess, result in guesses:
+        print(result)
+
+if __name__ == '__main__':
+    main()
